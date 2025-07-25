ee4f884f0cbd1431b39cb3877289f79f91417e32
Chintal77 <shandilyachintu90@gmail.com>
2025-07-26 00:25:22 +0530

    First Commit of my self learning journey !!!

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href=" ">ShopFusion</a>
+      </header>
+
+      <main>Product Lists</main>
+    </div>
+  );
+}
+
+export default App;
