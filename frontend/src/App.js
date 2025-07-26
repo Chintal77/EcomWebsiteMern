9cdae8648ede71a9626a5ce40de2376358d9cb11
Chintal77 <shandilyachintu90@gmail.com>
2025-07-26 21:14:27 +0530

    some changes in css file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +8,24 @@ function App() {
         <a href=" ">ShopFusion</a>
       </header>
 
-      <main>Product Lists</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <span className="badge">20% OFF</span>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <h3 className="product-name">{product.name}</h3>
+                <p className="product-price">
+                  ₹{product.price.toLocaleString('en-IN')}
+                </p>
+                <button className="add-to-cart">Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
