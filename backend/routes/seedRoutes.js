9de38adebf3e32849ddf9ed020be39827cb8ff89
Chintal77 <shandilyachintu90@gmail.com>
2025-07-28 22:33:04 +0530

    basic change

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-
-import data from '../data.js';
 import Product from '../models/productModel.js';
+import data from '../data.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({});
+  await Product.deleteMany(); // ✅ Clear existing entries
+  await Product.deleteOne({});
   const createdProducts = await Product.insertMany(data.products);
+
   res.send({ createdProducts });
 });
-
 export default seedRouter;
